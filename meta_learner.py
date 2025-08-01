64bda94d84d1dd8a13cecad48a4906eae0cfc794
Endimionx <endimion7@gmail.com>
2025-08-01 11:24:47 +0700

    Update meta_learner.py

--- meta_learner.py
+++ meta_learner.py
@@ -1,28 +1,18 @@
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
 
 class MetaLearner:
     def __init__(self):
-        self.models = [RandomForestClassifier(n_estimators=50) for _ in range(4)]
-
-    def train(self, data, labels):
-        # data: list of shape [n_samples, 4, n_features]
-        # labels: list of shape [n_samples, 4]
-        data = np.array(data)
-        labels = np.array(labels)
-        for i in range(4):
-            X = data[:, i, :]
-            y = labels[:, i]
-            self.models[i].fit(X, y)
+        pass  # No model needed
 
     def predict_top3(self, input_features):
-        # input_features: list of 4 arrays [LSTM, TRF, MARKOV] per posisi
-        result_preds, result_confs = [], []
-        for i in range(4):
-            feat = np.array(input_features[i]).reshape(1, -1)
-            prob = self.models[i].predict_proba(feat)[0]
-            prob = prob / prob.sum()
-            top3 = np.argsort(prob)[-3:][::-1]
-            result_preds.append(top3.tolist())
-            result_confs.append(prob[top3].tolist())
-        return result_preds, result_confs
+        preds, confs = [], []
+        for feats in input_features:
+            # feats: [LSTM(10), TRF(10), Markov(10)]
+            lstm = feats[:10]
+            trf = feats[10:20]
+            mkv = feats[20:]
+            combined = 0.4 * lstm + 0.4 * trf + 0.2 * mkv
+            top3 = np.argsort(combined)[-3:][::-1]
+            preds.append(top3.tolist())
+            confs.append(combined[top3].tolist())
+        return preds, confs
